4b981f457f694ba276214dd6c18ab7afd7134098
Luke <luke.spivak@edu.sait.ca>
2023-10-18 15:13:11 -0600

    added commit

--- temp.py
+++ temp.py
@@ -1,7 +1,8 @@
 def celsius_to_fahrenheit(celsius):
+    """given a celsius value returne the conversation"""
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
 
 celsius = 25
 fahrenheit = celsius_to_fahrenheit(celsius)
-print(f"{celsius}celsius to fahrenhiet is {fahrenheit}")
+print(f"{celsius} celsius to fahrenhiet is {fahrenheit}")
