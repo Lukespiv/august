0f68f5ae18010acd925695b66b4218474c11c909
Luke <luke.spivak@edu.sait.ca>
2023-10-18 15:23:38 -0600

    added another function to do fahrenheit to celsius

--- temp.py
+++ temp.py
@@ -3,6 +3,16 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
 
+def fahrenheit_to_celsius(fahrenheit):
+    """given a fahrenheit value returne the converted value"""
+    fahrenheit = 82
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
 celsius = 25
+
 fahrenheit = celsius_to_fahrenheit(celsius)
 print(f"{celsius} celsius to fahrenhiet is {fahrenheit}")
+
+celsius = fahrenheit_to_celsius(fahrenheit)
+print(f"{fahrenheit} fahrenhiet to celsius is {celsius}")
